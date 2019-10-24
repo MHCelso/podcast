0731f961729bc9a51fa7fc52c5b6fd8d005d77c8
mhcelso <furuyamic@gmail.com>
2019-10-24 10:40:29 -0500

    creacion de nuestra vista basada en clase

--- podcastproject/apps/index/views.py
+++ podcastproject/apps/index/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+"""
+[summary]
 
-# Create your views here.
+[description]
+"""
+
+# Django 
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Crea tus vistas aqui.
+class Index(TemplateView):
+	"""
+	[summary]
+	
+	[description]
+	
+	Extends:
+		TemplateView
+	""" 
+	# aqui el nombre del template este se encuentra a nivel aplicacion
+	template_name = 'index.html' 
+
+	def index_view(request):
+		"""
+		[summary]
+		[description: renderiza nuestro template]
+		
+		Arguments:
+			request {[type]} -- [description]
+		
+		Returns:
+			[type] -- [description]
+		"""
+		return render(template_name)
